7378820d356d7e16341683207ab13fac181121a5
Hjwasim <61841901+hjwasim@users.noreply.github.com>
2021-07-19 12:31:52 +0530

    REBUILD V2

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,18 +1,10 @@
 import React from 'react'
 import { hjwasim } from '../data'
-import { motion, useViewportScroll } from "framer-motion"
+import { motion } from "framer-motion"
 
 const Profile = () => {
-
-    const { scrollYProgress } = useViewportScroll()
-
-
     return (
         <div className="main">
-            <motion.path
-                d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
-                style={{ pathLength: scrollYProgress }}
-            />
             <div className="container py-4">
                 <motion.div animate={{ y: 0 }} initial={{ y: -100 }} className="card p-3 d-flex mt-4 mb-2 flex-column align-items-center justify-content-center">
                     <img alt="" className="profile_dp" src={hjwasim.avatar} />
